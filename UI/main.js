770f32454d20c5f827f947b58577ca0b3b2928df
Craig Wurld <85875550+dCOMMICS@users.noreply.github.com>
2023-06-24 07:13:25 -0700

    🤞

--- UI/main.js
+++ UI/main.js
@@ -51,3 +51,4 @@ points.push( new THREE.Vector3( 0, 10, 0 ) );
 points.push( new THREE.Vector3( 10, 0, 0 ) );
 
 const geometry = new THREE.BufferGeometry().setFromPoints( points );
+const line = new THREE.Line( geometry, material );
