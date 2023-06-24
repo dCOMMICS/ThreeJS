4a1c850f77802334c994de04c9998c75db7ea295
Craig Wurld <85875550+dCOMMICS@users.noreply.github.com>
2023-06-24 07:18:39 -0700

    loop

--- UI/vr.js
+++ UI/vr.js
@@ -3,3 +3,10 @@ import { VRButton } from 'three/addons/webxr/VRButton.js';
 document.body.appendChild( VRButton.createButton( renderer ) );
 //THIS CODE ENABLE XR RENDERING
 renderer.xr.enabled = true;
+
+// ANIMATION LOOP
+renderer.setAnimationLoop( function () {
+
+	renderer.render( scene, camera );
+
+} );
