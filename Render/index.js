efaed58e7aecded333873ac07ee9008158ffa902
Craig Wurld <85875550+dCOMMICS@users.noreply.github.com>
2023-06-24 07:52:07 -0700

    idk

--- Render/index.js
+++ Render/index.js
@@ -49,3 +49,13 @@ const Model = () => {
 }
 
 export default App;
+
+
+<Canvas shadows dpr={[1, 2]} camera={{ position: [0, 0, 4], fov: 50 }}>
+          <ambientLight intensity={0.7} />
+          <spotLight intensity={0.5} angle={0.1} penumbra={1} position={[10, 15, 10]} castShadow />
+          <Suspense fallback={null}>
+            <Model />
+            <Environment preset="city" />
+          </Suspense>
+</Canvas>
