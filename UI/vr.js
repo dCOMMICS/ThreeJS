f056763042a1f9e9d70d1e168575b574b4472d3e
Craig Wurld <85875550+dCOMMICS@users.noreply.github.com>
2023-06-24 07:17:57 -0700

    XR RENDERING

--- UI/vr.js
+++ UI/vr.js
@@ -1,3 +1,5 @@
 import { VRButton } from 'three/addons/webxr/VRButton.js';
 
 document.body.appendChild( VRButton.createButton( renderer ) );
+//THIS CODE ENABLE XR RENDERING
+renderer.xr.enabled = true;
